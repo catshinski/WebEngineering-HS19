7040128787c7056048b1a2c8c26b0e3413f0f1fe
catshinski <peter.zeltner@hotmail.com>
2019-10-07 10:54:22 +0200

    änderung auf error constraints

--- grails-app/controllers/mvc/InPlaceCalculatorController.groovy
+++ grails-app/controllers/mvc/InPlaceCalculatorController.groovy
@@ -1,15 +1,18 @@
 package mvc
 
+import grails.validation.Validateable
+import org.hibernate.validator.internal.metadata.facets.Validatable
+
 class InPlaceCalculatorController {
 
     def calc(CalculatorModel model) {
         model.en = Math.round(model.en * 10) / 10
         model.exam = Math.round(model.exam * 10) / 10
         model.result = Math.round((model.en + model.exam) / 2)
-        if (0.0 == model.en) {
+        if(model.errors.fieldErrors.any {it.field == "en"}){
             model.result = "Cannot calculate. En value was invalid."
         }
-        if (0.0 == model.exam) {
+        if (model.errors.fieldErrors.any {it.field == "exam"}) {
             model.result = "Cannot calculate. Exam value was invalid."
         }
         if(0.0 == model.en && 0.0 == model.exam){
@@ -19,9 +22,15 @@ class InPlaceCalculatorController {
     }
 }
 
-class CalculatorModel {
+class CalculatorModel implements Validateable{
 
     double en = 0.0
     double exam = 0.0
     String result = ""
+
+    static constraints = {
+        en min:1d, max:6d, scale:1
+        exam min:1d, max:6d, scale:1
+        result nullable: true
+    }
 }
